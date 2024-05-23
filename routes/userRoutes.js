108115919123fbc3f25e7d64acd83fd94f32add4
Avinashdwivedi41 <avinashd596@gmail.com>
2024-05-23 14:13:33 +0530

    final code

---
+++ routes/userRoutes.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const {
+  loginController,
+  registerController,
+  authController,
+  applyDoctorController,
+  getAllNotificationController,
+  deleteAllNotificationController,
+  getAllDocotrsController,
+  bookeAppointmentController,
+  bookingAvailabilityController,
+  userAppointmentsController,
+} = require("../controllers/userCtrl");
+const authMiddleware = require("../middlewares/authMiddleware");
+
+//router onject
+const router = express.Router();
+
+//routes
+//LOGIN || POST
+
+
+//REGISTER || POST
+router.post("/register", registerController);
+router.post("/login", loginController);
+
+//Auth || POST
+router.post("/getUserData", authMiddleware, authController);
+
+//APply Doctor || POST
+router.post("/apply-doctor", authMiddleware, applyDoctorController);
+
+//Notifiaction  Doctor || POST
+router.post(
+  "/get-all-notification",
+  authMiddleware,
+  getAllNotificationController
+);
+//Notifiaction  Doctor || POST
+router.post(
+  "/delete-all-notification",
+  authMiddleware,
+  deleteAllNotificationController
+);
+
+//GET ALL DOC
+router.get("/getAllDoctors", authMiddleware, getAllDocotrsController);
+
+//BOOK APPOINTMENT
+router.post("/book-appointment", authMiddleware, bookeAppointmentController);
+
+//Booking Avliability
+router.post(
+  "/booking-availability",
+  authMiddleware,
+  bookingAvailabilityController
+);
+
+
+router.get('/user-appointments',authMiddleware,userAppointmentsController)
+
+module.exports = router;
